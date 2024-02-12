7cd14cb220e39631868554d512afff3b15e1d5dc
zooyaam <yjoo2498@naver.com>
2024-02-13 00:04:30 +0900

    [기능] data 렌더링

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,13 +1,25 @@
-export default function AddTodo() {
+import { useState } from "react";
+
+export default function AddTodo({ onAddTodo }) {
+  const [title, setTitle] = useState("");
+
   return (
     <div className="mt-8 p-5 bg-background w-[30rem] h-52 rounded-lg">
       <div className="grid gap-[23px] grid-rows-3 w-full">
         <input
           type="text"
-          placeholder="AddTodo"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="What needs to be done?"
           className="p-2 w-full outline-none border rounded-lg"
         />
-        <button className="bg-tertiary w-full p-2 rounded-lg justify-self-end row-start-3">
+        <button
+          onClick={() => {
+            setTitle("");
+            onAddTodo(title);
+          }}
+          className="bg-tertiary w-full p-2 rounded-lg justify-self-end row-start-3 hover:bg-[#E6D4B8] transition-all duration-300"
+        >
           Add
         </button>
       </div>
--- src/components/Contents/Contents.jsx
+++ src/components/Contents/Contents.jsx
@@ -1,19 +1,26 @@
+import taskList from "../../data/todo.json";
+
 export default function Contents() {
   return (
     <div className="">
       <button className="bg-background px-8 py-1 rounded-t-lg">Todo</button>
-      <button className="bg-tertiary px-8 py-1 rounded-t-lg">Checked</button>
+      <button className="bg-tertiary px-8 py-1 rounded-t-lg">Completed</button>
       <button className="bg-tertiary ml-[371px] px-8 py-1 rounded-t-lg">
-        Task
+        All
       </button>
       <div className="bg-background w-[45rem] h-96 rounded-tr-lg rounded-b-lg px-6 py-8">
-        <div className="py-2 grid grid-cols-[32px_auto_64px] items-center">
-          <input type="checkbox" className="size-4" />
-          <label className="text-label-sm">윤이 쓰다듬기</label>
-          <button className="bg-tertiary w-16 h-7 rounded-lg justify-self-end">
-            Done
-          </button>
-        </div>
+        {taskList.todos.map(({ title }) => (
+          <div
+            key={title}
+            className="py-2 grid grid-cols-[32px_auto_64px] items-center"
+          >
+            <input type="checkbox" className="size-4" />
+            <label className="text-label-sm">{title}</label>
+            <button className="bg-tertiary w-16 h-7 rounded-lg justify-self-end hover:bg-[#E6D4B8] transition-all duration-300">
+              Delete
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
